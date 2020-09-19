b582e0132a4e9c8a8f6c5594e994cf5bfa2d7c21
Emmanuel Gomes <aeag@outlook.com>
2020-09-19 20:38:11 -0300

    propTypes and styled-components

--- front-end/src/pages/SignIn/index.js
+++ front-end/src/pages/SignIn/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SignIn = () => {
-  return <div />;
+  return <h1>login sign in</h1>;
 };
 
 export default SignIn;
---
+++ front-end/src/pages/_layouts/auth/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-90deg, #c09517, #ab59c1);
+`;
